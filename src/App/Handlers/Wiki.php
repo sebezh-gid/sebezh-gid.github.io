1ee295ceca0279145b356c530944dffa9532717e
Justin Forest <hex@umonkey.net>
2018-11-11 17:59:17 +0300

    Page: refresh on debug

--- src/App/Handlers/Wiki.php
+++ src/App/Handlers/Wiki.php
@@ -248,6 +248,9 @@ class Wiki extends CommonHandler
      **/
     protected function refresh(Request $request)
     {
+        if ($request->getParam("debug"))
+            return true;
+
         $headers = $request->getHeaders();
 
         $cacheControl = @$headers["HTTP_CACHE_CONTROL"][0];
@@ -301,5 +304,123 @@ class Wiki extends CommonHandler
         }
 
         return $image;
+    }
+
+    /**
+     * Process wiki page syntax.
+     *
+     * @param string $name Page name.
+     * @param string $source Page source.
+     * @return array Page properties: title, html, etc.
+     **/
+    protected function processWikiPage($name, $source)
+    {
+        $res = [
+            "name" => $name,
+            "title" => $name,
+            "image" => null,
+            "summary" => null,
+        ];
+
+        $source = str_replace("\r\n", "\n", $source);
+        $blocks = explode("\n---\n", $source, 2);
+
+        if (count($blocks) == 2) {
+            if (preg_match_all('@^([^:]+):\s*(.+)$@m', $blocks[0], $m)) {
+                foreach ($m[1] as $idx => $key)
+                    $res[$key] = $m[2][$idx];
+            }
+
+            $source = $blocks[1];
+        }
+
+        // Process wiki links.
+        $source = preg_replace_callback('@\[\[([^]]+)\]\]@', function ($m) {
+            // Embed images later.
+            if (0 === strpos($m[1], "image:"))
+                return $m[0];
+
+            $link = $m[1];
+            $label = $m[1];
+
+            if (count($parts = explode("|", $m[1], 2)) == 2) {
+                $link = $parts[0];
+                $label = $parts[1];
+            }
+
+            $cls = "good";
+            if (empty($this->db->fetch("SELECT `name` FROM `pages` WHERE `name` = ?", [$link])))
+                $cls = "broken";
+
+            $html = sprintf("<a href='/wiki?name=%s' class='wiki %s'>%s</a>", urlencode($link), $cls, htmlspecialchars($label));
+
+            return $html;
+        }, $source);
+
+        $html = \App\Common::renderMarkdown($source);
+        $html = \App\Common::renderTOC($html);
+
+        // Embed images.
+        $html = preg_replace_callback('@\[\[image:([^]]+)\]\]@', function ($m) use ($name) {
+            $parts = explode(":", $m[1]);
+            $fileId = array_shift($parts);
+
+            $info = $this->db->fetchOne("SELECT `id`, `type`, `kind` FROM `files` WHERE `id` = ?", [$fileId]);
+            if (empty($info))
+                return "<!-- file {$fileid} does not exist -->";
+            elseif ($info["kind"] != "photo")
+                return "<!-- file {$fileid} is not an image -->";
+
+            $className = "image";
+            $iw = "auto";
+            $ih = "auto";
+
+            foreach ($parts as $part) {
+                if (preg_match('@^width=(\d+)$@', $part, $m)) {
+                    $iw = $m[1] . "px";
+                }
+
+                elseif (preg_match('@^height=(\d+)$@', $part, $m)) {
+                    $ih = $m[1] . "px";
+                }
+
+                else {
+                    $className .= " " . $part;
+                }
+            }
+
+            if ($iw == "auto" and $ih == "auto")
+                $ih = "150px";
+
+            $small = "/i/thumbnails/{$fileId}.jpg";
+            $large = "/i/photos/{$fileId}.jpg";
+            $page = "/wiki?name=File:{$fileId}";
+
+            // TODO: add caption from page
+            // TODO: add lazy loading
+
+            $html = "<a class='{$className}' href='{$page}' data-src='{$large}' data-fancybox='gallery' itemscope itemtype='http://schema.org/ImageObject'>";
+            $html .= "<meta itemprop='contentUrl' content='{$large}'/>";
+            $html .= "<img src='{$small}' style='width: {$iw}; height: {$ih}' itemprop='thumbnailUrl'/>";
+            $html .= "</a>";
+
+            return $html;
+        }, $html);
+
+        $html = preg_replace_callback('@<h1>(.+)</h1>@', function ($m) use (&$res) {
+            $res["title"] = $m[1];
+            return "";
+        }, $html);
+
+        if (empty($res["summary"])) {
+            if (preg_match('@<p>(.+?)</p>@', $html, $m)) {
+                $res["summary"] = strip_tags($m[1]);
+            }
+        }
+
+        $html = \App\Util::cleanHtml($html);
+        $res["html"] = $html;
+
+        return $res;
     }
 }
