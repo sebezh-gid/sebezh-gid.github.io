41632d2025e25beedff7eb50160745324429fa5b
Justin Forest <hex@umonkey.net>
2020-12-05 05:28:34 +0300

    Remove qrcodes

--- src/routes.php
+++ src/routes.php
@@ -35,16 +35,6 @@ $app->get ('/map/all.json', '\App\Handlers\Maps:onAllJSON');
 $app->get ('/map/points.json', '\App\Handlers\Maps:onPoints');
 $app->post('/map/suggest-ll', '\App\Handlers\Maps:onSuggestLL');
 
-$app->get ('/short', '\App\Handlers\Short:onGetForm');
-$app->post('/short', '\App\Handlers\Short:onCreate');
-$app->get ('/short/preview', '\App\Handlers\Short:onGetPreview');
-$app->get ('/short/{code:[0-9]{4}}', '\App\Handlers\Short:onShowItem');
-$app->get ('/i/short/{code:[0-9]{4}}.png', '\App\Handlers\Short:onGetImage');
-$app->get ('/s/{code:[0-9]{4}}', '\App\Handlers\Short:onRedirect');
-
-$app->get ('/short/{code:[0-9]{4}}.png', '\App\Handlers\ShortImage');
-$app->get ('/{code:[0-9]{4}}', '\App\Handlers\ShortRedirect');
-
 $app->any('/upload', '\App\Handlers\Upload');
 
 $app->get ('/files/export', '\App\Handlers\Files:onExport');
