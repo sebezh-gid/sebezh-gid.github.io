46d60da96184253f9ae73c5b1015ed2c8e1c8bdb
Justin Forest <hex@umonkey.net>
2021-02-21 19:02:31 +0300

    Add caching middleware
    Adds Cache-Control header if not set.
    Only for GET requests.

---
+++ config/middleware.php
@@ -0,0 +1,5 @@
+<?php
+
+declare(strict_types=1);
+
+$app->add('App\Cache\CachingMiddleware');
---
+++ src/Cache/CachingMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Cache;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+class CachingMiddleware
+{
+    public function __invoke(Request $request, RequestHandler $handler): Response
+    {
+        $response = $handler->handle($request);
+
+        if ($request->getMethod() === 'GET') {
+            if (!$this->hasCacheControl($response)) {
+                $response = $response->withHeader('Cache-Control', 'public, max-age=3600');
+            }
+        }
+
+        return $response;
+    }
+
+    protected function hasCacheControl(Response $response): bool
+    {
+        $headers = $response->getHeader('cache-control');
+        return count($headers) > 0;
+    }
+}
